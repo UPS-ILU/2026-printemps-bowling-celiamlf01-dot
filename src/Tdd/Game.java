49d924bdcb2e60e707a53926581a4cfe2c676236
Cylia Makhlouf <mkc5351a@ensfsi.onmicrosoft.com>
2026-04-17 15:34:09 +0200

    test3

--- src/Tdd/Game.java
+++ src/Tdd/Game.java
@@ -1,12 +1,24 @@
 package Tdd;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Game {
+	static private int[] rolls = new int[21];
+	private int iter=0;
+	
 	void roll(int nbQuilles)
 	{
-		
+		rolls[iter]=nbQuilles;
+		++iter;
 	}
 	int score() { 
-		return 0;
+		int scorep=0;
+		for(int i=0;i<20;++i)
+		{
+			scorep+=rolls[i];
+		}
+	
+		return scorep;
 	}
 
 }
--- test/Tdd/GameTest.java
+++ test/Tdd/GameTest.java
@@ -9,19 +9,33 @@ class GameTest {
 
 	Game game;
 
+
 	@BeforeEach
 	void init() {
 		game = new Game();
 	}
 
 	@Test
-	void test_ausune_quille() {
-		for(int i=0;i<20;++i) {
+	void test_aucune_quille() {
+		for (int i = 0; i < 20; ++i)
 			game.roll(0);
-			assertEquals(0,game.score());
-			
-		}
-		
+		assertEquals(0, game.score());}
+	@Test
+	void test_une_quille() {
+		for (int i = 0; i < 20; ++i)
+			game.roll(1);
+		assertEquals(20, game.score());
+
 	}
+	@Test
+	void test_30_quille() {
+		for (int i = 0; i < 10; ++i)
+			game.roll(1);
+		for (int i = 10; i < 20; ++i)
+			game.roll(2);
+		assertEquals(30, game.score());
+
+	}
+
 
 }
